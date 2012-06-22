bafae4189eb4fe386ed86cb594d5c282f5910a85
Rich Siegel <rsiegel@ise.com>
2012-06-21 22:21:57 -0400

    Initial build of instances,query,latest

--- lib/puppet/provider/package/chocolatey.rb
+++ lib/puppet/provider/package/chocolatey.rb
@@ -6,14 +6,39 @@ Puppet::Type.type(:package).provide :chocolatey, :parent => Puppet::Provider::Pa
   commands :chocolatey => "C:/Chocolatey/chocolateyInstall/chocolatey.cmd"
   confine    :operatingsystem => :windows
   
- 
-  def self.instances
-    # TODO:  This is very hard 
-    # self.instances is a class method of the provider that returns all of 
+  def self.parse(line)
+    #parse everything with foo==ver
+    if line.chomp =~ /^([^=]+)==([^=]+)$/
+      {:ensure => $2, :name => $1, :provider => name}
+    else
+      nil
+    end
+  end
+
+  # from puppet-dev mailing list:
+  # self.instances is a class method of the provider that returns all of 
 	# the resources present on the system. Not just a specific resource. 
-	# If you are working with a new package manager, then self.instances 
-	# will return a array of hashes. Each element in the array describing a single package. 
+	# then self.instances will return a array of hashes. Each element in the array describing a single package. 
 	
+  
+  def self.instances
+    packages = []
+    
+    # the following:
+    # chocolatey version all -lo | % { "{0}=={1}" -f $_.Name, $_.Found }
+    # returns
+    #(name==version)
+    
+    subcommand = "version"
+    package = "all"
+    args = = subcommand + " " + package + " " + %q{-lo | % { "{0}=={1}" -f $_.Name, $_.Found }}    
+    chocolatey(*args) do |process|
+      process.collect do |line|
+        next unless options = parse(line)
+        packages << new(options)
+      end
+    end
+    packages
   end
 
   
@@ -23,35 +48,40 @@ Puppet::Type.type(:package).provide :chocolatey, :parent => Puppet::Provider::Pa
     chocolatey(*args)
   end
 
-  def query
   
-		# Puppet will call the query method on the instance of the package 
+    # from puppet-dev mailing list
+  	# Puppet will call the query method on the instance of the package 
 		# provider resource when checking if the package is installed already or 
-		# not. It's a determination for one specific package, the package modeled by 
+		# not.
+    # It's a determination for one specific package, the package modeled by 
 		# the resource the method is called on. 
-
 		# Query provides the information for the single package identified by @Resource[:name]. 
 
-   
-    chocolatey version @resource[:name]
-    {:ensure => :present}
-  rescue
-    {:ensure => :absent}
+  def query
+    self.class.instances.each do |provider_chocolatey|
+      return provider_chocolatey.properties if @resource[:name] == provider_chocolatey.name
+    end
+    return nil
   end
 
+   
   def uninstall
     args = "uninstall" + @resource[:name]
     chocolatey(*args)
   end
 
-
+def latest
+    # force chocolatey version output formatting (name==latest)
+    package = @resource[:name]
+    subcommand = "version"
+    args = subcommand + " " + package + " " + %q{| % { "{0}=={1}" -f $_.Name, $_.Latest }}    
+    chocolatey(*args) do |process|
+      process.collect do |line|
+        next unless options = parse(line)
+        packages << new(options)
+      end
+    end
+    packages
+  end
+  
 end
-  
-  
-  
-  
-
-
-
-
-
