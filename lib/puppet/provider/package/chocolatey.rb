4000890ef6053482d27d38d949bbe2c6208d94cb
Rich Siegel <rsiegel@ise.com>
2012-10-18 23:38:03 -0400

    commented out test verbosity notice

--- lib/puppet/provider/package/chocolatey.rb
+++ lib/puppet/provider/package/chocolatey.rb
@@ -22,19 +22,19 @@ Puppet::Type.type(:package).provide(:chocolatey, :parent => Puppet::Provider::Pa
     args = "uninstall", @resource[:name]
     chocolatey(*args)
   end
-  
+
   def update
     args = "update", @resource[:name]
     chocolatey(*args)
   end
-  
-    # from puppet-dev mailing list
-  	# Puppet will call the query method on the instance of the package 
-		# provider resource when checking if the package is installed already or 
-		# not.
-    # It's a determination for one specific package, the package modeled by 
-		# the resource the method is called on. 
-		# Query provides the information for the single package identified by @Resource[:name]. 
+
+  # from puppet-dev mailing list
+  # Puppet will call the query method on the instance of the package 
+  # provider resource when checking if the package is installed already or 
+  # not.
+  # It's a determination for one specific package, the package modeled by 
+  # the resource the method is called on. 
+  # Query provides the information for the single package identified by @Resource[:name]. 
     
     def query
     
@@ -45,30 +45,27 @@ Puppet::Type.type(:package).provide(:chocolatey, :parent => Puppet::Provider::Pa
     return nil
   end
 
-  
   def self.listcmd
-    
     [command(:chocolatey), ' version all -lo ^| % { \"{0}=={1}\" -f $_.Name, $_.Found }']
   end
 
-  
   def self.instances
     packages = []
-    
+
     begin
       execpipe(listcmd()) do |process|
-        
+
         regex = %r{^([^=]+)==([^=]+)$}
         fields = [:name, :ensure]
         hash = {}
-       
+
         process.each_line { |line|
           if match = regex.match(line)
             fields.zip(match.captures) { |field,value|
               hash[field] = value
           }
             name = hash[:name]
-            notice "name #{name}"
+            #notice "name #{name}"
             hash[:provider] = self.name
             packages << new(hash)
             hash = {}
@@ -86,17 +83,17 @@ Puppet::Type.type(:package).provide(:chocolatey, :parent => Puppet::Provider::Pa
   def latestcmd
     [command(:chocolatey), ' version ' + @resource[:name] + ' ^| % { \"{0}=={1}\" -f $_.Name, $_.Latest }']
   end
-  
+
   def latest
     packages = []
-    
+
     begin
       output = execpipe(latestcmd()) do |process|
         puts "#{output}"
         regex = %r{^([^=]+)==([^=]+)$}
         fields = [:name, :latest]
         hash = {}
-       
+
         process.each_line { |line|
           if match = regex.match(line)
             fields.zip(match.captures) { |field,value|
@@ -117,5 +114,5 @@ Puppet::Type.type(:package).provide(:chocolatey, :parent => Puppet::Provider::Pa
     end
     packages
   end
-  
+
 end
