68766c537e514e10ecc337b50b95579128667f84
Rob Reynolds <ferventcoder@gmail.com>
2015-02-28 08:05:06 -0600

    (PUP-401) Skeleton for config/install
    Add skeleton files generated from puppet module generate using
    @garethr module skeleton -
    https://github.com/garethr/puppet-module-skeleton

---
+++ manifests/config.pp
@@ -0,0 +1,6 @@
+# == Class chocolatey::config
+#
+# This class is called from chocolatey for service config.
+#
+class chocolatey::config {
+}
---
+++ manifests/install.pp
@@ -0,0 +1,10 @@
+# == Class chocolatey::install
+#
+# This class is called from chocolatey for install.
+#
+class chocolatey::install {
+
+  # package { $::chocolatey::package_name:
+  #   ensure => present,
+  # }
+}
