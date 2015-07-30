114b131c05c6a0b44357250a6bef3db49b72c439
Rob Reynolds <ferventcoder@gmail.com>
2015-07-30 17:27:24 -0500

    (PUP-1691) Install Chocolatey
    Allow installing chocolatey with a custom package location. Also allow
    using the built-in windows shell to unzip the package. The original request for
    this had the ability to use the built-in Windows shell to perform the unzipping
    and it keeps folks from having to download an external executable by
    default. Allow 25 minutes for it to finish.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,11 +1,31 @@
 # == Class chocolatey::install
 #
 # This class is called from chocolatey for install.
-#
-# NOTE: CURRENTLY NON-FUNCTIONAL
-class chocolatey::install {
+class chocolatey::install (
+  $chocolatey_download_url = 'https://chocolatey.org/api/v2/package/chocolatey/',
+  $use_7zip                = false,
+  $choco_install_location  = 'C:\ProgramData\chocolatey'
+){
+  # todo:
+  # - allow custom installation directory to be specified
+  #   - set the fact that sets the chocolateyInstall Variable in the path
+  # - restrict based on osfamily?
 
-  # package { $::chocolatey::package_name:
-  #   ensure => present,
-  # }
+  $download_url = $chocolatey_download_url
+  $unzip_type   = $use_7zip ? {
+    true     => '7zip',
+    default  => 'windows'
+  }
+
+  # call for the custom fact to be set
+
+  exec { 'install_chocolatey_official':
+    command  => template('chocolatey/InstallChocolatey.ps1.erb'),
+    creates  => "${choco_install_location}\\bin\\choco.exe",
+    provider => powershell,
+    timeout  => 1500,
+  }
+
+  # we'll need a trick to update path once we run
+
 }
