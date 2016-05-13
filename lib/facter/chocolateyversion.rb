cff2a7dbe367ded1c1291265bd12fbe6a8b97013
Rob Reynolds <ferventcoder@gmail.com>
2016-05-13 15:41:35 -0500

    (maint) ChocolateyVersion common
    Move the bits for the fact `chocolateyversion` to
    `PuppetX::Chocolatey::ChocolateyVersion`. Move the tests around and
    update appropriately as well.
    
    Also when working against 0.9.8.33, handle the warning message
    that is shown to users before the version message.

--- lib/facter/chocolateyversion.rb
+++ lib/facter/chocolateyversion.rb
@@ -1,18 +1,8 @@
+require 'pathname'
+require Pathname.new(__FILE__).dirname + '../' + 'puppet_x/chocolatey/chocolatey_version'
+
 Facter.add('chocolateyversion') do
   setcode do
-    value = nil
-
-    choco_path = "#{Facter.value(:choco_install_path)}\\bin\\choco.exe"
-    if Puppet::Util::Platform.windows? && File.exist?(choco_path)
-      begin
-        old_choco_message = 'Please run chocolatey /? or chocolatey help - chocolatey v'
-        #Facter::Core::Execution.exec is 2.0.1 forward
-        value = Facter::Util::Resolution.exec("#{choco_path} -v").gsub(old_choco_message,'').strip
-      rescue StandardError => e
-        value = '0'
-      end
-    end
-
-    value || '0'
+    PuppetX::Chocolatey::ChocolateyVersion.version
   end
 end
--- spec/unit/facter/chocolateyversion_spec.rb
+++ spec/unit/facter/chocolateyversion_spec.rb
@@ -1,35 +1,20 @@
+require 'spec_helper'
 require 'facter'
+require 'puppet_x/chocolatey/chocolatey_version'
 
 describe 'chocolateyversion fact' do
   subject(:fact) { Facter.fact(:chocolateyversion) }
 
-  context 'on Windows', :if => Puppet::Util::Platform.windows? do
-    it "should return the value from running choco -v" do
-      expected_value = '1.2.3'
-      Facter::Util::Resolution.expects(:exec).returns(expected_value)
-
-      subject.value.must == expected_value
-    end
-
-    it "should handle cleaning up spaces" do
-      expected_value = '1.2.3'
-      Facter::Util::Resolution.expects(:exec).returns(' ' + expected_value + ' ')
-
-      subject.value.must == expected_value
-    end
-
-    it "should handle older versions of choco" do
-      expected_value = '1.2.3'
-      Facter::Util::Resolution.expects(:exec).returns('Please run chocolatey /? or chocolatey help - chocolatey v' + expected_value)
-
-      subject.value.must == expected_value
-    end
+  before :each do
+    Facter.clear
+    Facter.clear_messages
   end
 
-  context 'on Linux', :if => Puppet.features.posix? do
-    it "should return 0  on a non-windows system" do
-      subject.value.must == "0"
-    end
+  it "should return the output of PuppetX::Chocolatey::ChocolateyVersion.version" do
+    expected_value = '1.2.3'
+    PuppetX::Chocolatey::ChocolateyVersion.expects(:version).returns(expected_value)
+
+    subject.value.must == expected_value
   end
 
   after :each do
