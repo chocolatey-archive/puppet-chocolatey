1376d3d482ddf048ad314602f887a3921728c3a4
Rob Reynolds <ferventcoder@gmail.com>
2016-05-17 18:52:34 -0500

    (maint) ChocolateyInstall should check environment
    If a registry check does not turn up results, ChocolateyInstall should
    fall back to looking at process/user environment variable to determine
    if the environment variable `ChocolateyInstall` is defined.

--- lib/puppet_x/chocolatey/chocolatey_install.rb
+++ lib/puppet_x/chocolatey/chocolatey_install.rb
@@ -3,10 +3,11 @@ module PuppetX
     class ChocolateyInstall
 
       def self.install_path
+        value = nil
+
         if Puppet::Util::Platform.windows?
           require 'win32/registry'
 
-          value = nil
           begin
             hive = Win32::Registry::HKEY_LOCAL_MACHINE
             hive.open('SYSTEM\CurrentControlSet\Control\Session Manager\Environment', Win32::Registry::KEY_READ | 0x100) do |reg|
@@ -17,7 +18,16 @@ module PuppetX
           end
         end
 
-        value || 'C:\ProgramData\chocolatey'
+        # If machine level is not set, use process or user as the intended
+        # location where Chocolatey would be installed.
+        # Since it is technically possible that Chocolatey could exist on
+        # non-Windows installations, we don't want to confine this
+        # to just Windows.
+        if value.nil?
+          value = ENV['ChocolateyInstall']
+        end
+
+        value
       end
     end
   end
--- spec/unit/puppet_x/chocolatey/chocolatey_install_spec.rb
+++ spec/unit/puppet_x/chocolatey/chocolatey_install_spec.rb
@@ -12,17 +12,41 @@ describe 'Chocolatey Install Location' do
       PuppetX::Chocolatey::ChocolateyInstall.install_path.must == expected_value
     end
 
-    it "should return the default install path if environment variable does not exist" do
-      expected_value = 'C:\ProgramData\chocolatey'
+    it "should return the environment variable ChocolateyInstall if it exists" do
       Win32::Registry.any_instance.expects(:[]).with('ChocolateyInstall').raises(Win32::Registry::Error.new(2), 'file not found yo')
 
-      PuppetX::Chocolatey::ChocolateyInstall.install_path.must == expected_value
+      # this is a placeholder, it is already set in spec_helper
+      ENV['ChocolateyInstall'] = 'c:\blah'
+
+      PuppetX::Chocolatey::ChocolateyInstall.install_path.must == 'c:\blah'
+    end
+
+    it "should return nil if the environment variable does not exist" do
+      Win32::Registry.any_instance.expects(:[]).with('ChocolateyInstall').raises(Win32::Registry::Error.new(2), 'file not found yo')
+      ENV['ChocolateyInstall'] = nil
+
+      PuppetX::Chocolatey::ChocolateyInstall.install_path.must be_nil
     end
   end
 
   context 'on Linux', :if => Puppet.features.posix? do
-    it "should return the default install path on a non-windows system" do
-      PuppetX::Chocolatey::ChocolateyInstall.install_path.must == 'C:\ProgramData\chocolatey'
+    it "should return the environment variable ChocolateyInstall if it exists" do
+      # this is a placeholder, it is already set in spec_helper
+      ENV['ChocolateyInstall'] = 'c:\blah'
+
+      PuppetX::Chocolatey::ChocolateyInstall.install_path.must == 'c:\blah'
+    end
+
+    it "should return nil if the ChocolateyInstall variable does not exist" do
+      ENV['ChocolateyInstall'] = nil
+
+      PuppetX::Chocolatey::ChocolateyInstall.install_path.must be_nil
     end
   end
+
+  after :each do
+    # setting the values back
+    ENV['ChocolateyInstall'] = 'c:\blah'
+  end
+
 end
