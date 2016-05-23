1c858ba5458a5939f3a196b140485631b467c911
Rob Reynolds <ferventcoder@gmail.com>
2016-05-23 17:41:07 -0500

    (MODULES-3037) Confine install path fact w/default
    Confine the choco_install_path to Windows osfamily (taking the the
    example from other modules that do similar confines) and ensure that
    the default is passed back if ChocolateyInstall.install_path comes back
    nil.

--- spec/unit/facter/choco_install_path_spec.rb
+++ spec/unit/facter/choco_install_path_spec.rb
@@ -10,11 +10,20 @@ describe 'choco_install_path fact' do
     Facter.clear_messages
   end
 
-  it "should return the output of PuppetX::Chocolatey::ChocolateyInstall.install_path" do
-    expected_value = 'C:\somewhere'
-    PuppetX::Chocolatey::ChocolateyInstall.expects(:install_path).returns(expected_value)
+  context "on Windows", :if => Puppet::Util::Platform.windows? do
+    it "should return the output of PuppetX::Chocolatey::ChocolateyInstall.install_path" do
+      expected_value = 'C:\somewhere'
+      PuppetX::Chocolatey::ChocolateyInstall.expects(:install_path).returns(expected_value)
 
-    subject.value.must == expected_value
+      subject.value.must == expected_value
+    end
+
+
+    it "should return the default path when PuppetX::Chocolatey::ChocolateyInstall.install_path is nil" do
+      PuppetX::Chocolatey::ChocolateyInstall.expects(:install_path).returns(nil)
+
+      subject.value.must == 'C:\ProgramData\chocolatey'
+    end
   end
 
   after :each do
