84a9e2bcde6aff1f374c930013f64336350b3ab4
Rob Reynolds <ferventcoder@gmail.com>
2016-05-24 13:02:18 -0500

    (MODULES-3037) Confine version fact w/default
    Confine the chocolateyversion fact to Windows osfamily and ensure that
    the default of 0 is passed back if ChocolateyVersion.version comes back
    nil.

--- lib/facter/chocolateyversion.rb
+++ lib/facter/chocolateyversion.rb
@@ -2,7 +2,8 @@ require 'pathname'
 require Pathname.new(__FILE__).dirname + '../' + 'puppet_x/chocolatey/chocolatey_version'
 
 Facter.add('chocolateyversion') do
+  confine :osfamily => :windows
   setcode do
-    PuppetX::Chocolatey::ChocolateyVersion.version
+    PuppetX::Chocolatey::ChocolateyVersion.version || '0'
   end
 end
--- spec/unit/facter/chocolateyversion_spec.rb
+++ spec/unit/facter/chocolateyversion_spec.rb
@@ -10,11 +10,19 @@ describe 'chocolateyversion fact' do
     Facter.clear_messages
   end
 
-  it "should return the output of PuppetX::Chocolatey::ChocolateyVersion.version" do
-    expected_value = '1.2.3'
-    PuppetX::Chocolatey::ChocolateyVersion.expects(:version).returns(expected_value)
+  context "on Windows", :if => Puppet::Util::Platform.windows? do
+    it "should return the output of PuppetX::Chocolatey::ChocolateyVersion.version" do
+      expected_value = '1.2.3'
+      PuppetX::Chocolatey::ChocolateyVersion.expects(:version).returns(expected_value)
 
-    subject.value.must == expected_value
+      subject.value.must == expected_value
+    end
+
+    it "should return the default of 0 when PuppetX::Chocolatey::ChocolateyVersion.version is nil" do
+      PuppetX::Chocolatey::ChocolateyVersion.expects(:version).returns(nil)
+
+      subject.value.must == '0'
+    end
   end
 
   after :each do
