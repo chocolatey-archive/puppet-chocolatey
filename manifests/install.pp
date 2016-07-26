10535d9ddb5c72fa3e51d8a345ccba359a1645d3
Rob Reynolds <ferventcoder@gmail.com>
2016-07-26 17:11:16 -0500

    (MODULES-3641) Remove Windows_env module dependency
    It was originally required to set the environment variables prior
    to running Chocolatey install to help prepare the environment for
    suitability. It was originally added in 5a849adca and ad57d1429
    to ensure that Chocolatey could be suitable to be installed and
    used to install packages on the first run. This was also to
    ensure that you could override the Chocolatey install directory
    by setting the value in advance of the install.
    
    In b5034d167 @rismoney removed the necessity for pre-setting the
    environment variable as it was passed to the install exec.
    
    We have since determined that the providers default to a location
    of `$env:ALLUSERSPROFILE\chocolatey\bin\choco.exe` when an install
    path cannot be found. https://github.com/puppetlabs/puppetlabs-chocolatey/blob/fa67417bbfac06049f36346e02eb09dddb68084c/lib/puppet_x/chocolatey/chocolatey_common.rb#L31-L36
    This evaluates to the default install path of
    `C:\ProgramData\chocolatey\bin\choco.exe`, allowing suitability
    to work properly on the same converge as the install ONLY if you
    install to the default install location with Chocolatey. This
    has nothing to do with the hope that windows_env would update
    the current shell! So it is safe to remove windows_env without
    losing any of the current behavior.

--- manifests/install.pp
+++ manifests/install.pp
@@ -8,24 +8,6 @@ class chocolatey::install {
     default   => 'windows'
   }
 
-  # These are specifically necessary to ensure that we know the path
-  # has been updated in the current run. They are typically a noop.
-  windows_env { 'chocolatey_ChocolateyInstall_env':
-    ensure    => present,
-    variable  => 'ChocolateyInstall',
-    mergemode => 'clobber',
-    value     => $::chocolatey::choco_install_location,
-    notify    => Exec['install_chocolatey_official'],
-  }
-
-  windows_env { 'chocolatey_PATH_env':
-    ensure    => present,
-    variable  => 'PATH',
-    mergemode => 'prepend',
-    value     => "${::chocolatey::choco_install_location}\\bin",
-    notify    => Exec['install_chocolatey_official'],
-  }
-
   exec { 'install_chocolatey_official':
     command     => template('chocolatey/InstallChocolatey.ps1.erb'),
     creates     => "${::chocolatey::choco_install_location}\\bin\\choco.exe",
--- spec/classes/install_spec.rb
+++ spec/classes/install_spec.rb
@@ -10,21 +10,11 @@ RSpec.describe 'chocolatey' do
   }
 
   context 'contains install.pp' do
-    it { is_expected.to contain_windows_env('chocolatey_ChocolateyInstall_env').with_ensure('present') }
-    it { is_expected.to contain_windows_env('chocolatey_ChocolateyInstall_env').with_variable('ChocolateyInstall') }
-    it { is_expected.to contain_windows_env('chocolatey_ChocolateyInstall_env').that_notifies('Exec[install_chocolatey_official]') }
-
-    it { is_expected.to contain_windows_env('chocolatey_PATH_env').with_ensure('present') }
-    it { is_expected.to contain_windows_env('chocolatey_PATH_env').with_variable('PATH') }
-    it { is_expected.to contain_windows_env('chocolatey_PATH_env').that_notifies('Exec[install_chocolatey_official]') }
-
     ['c:\local_folder', "C:\\ProgramData\\chocolatey"].each do |param_value|
         context "choco_install_location => #{param_value}" do
         let(:params) {{ :choco_install_location => param_value }}
 
         it { is_expected.to contain_exec('install_chocolatey_official').with_creates("#{param_value}\\bin\\choco.exe") }
-        it { is_expected.to contain_windows_env('chocolatey_ChocolateyInstall_env').with_value("#{param_value}") }
-        it { is_expected.to contain_windows_env('chocolatey_PATH_env').with_value("#{param_value}\\bin") }
       end
     end
 
