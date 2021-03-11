114b0b7226fca63d83217c826f2d4e587ca24eee
David Schmitt <david.schmitt@puppet.com>
2021-03-11 10:18:09 +0000

    Clean up old compatibility code

--- spec/spec_helper_local.rb
+++ spec/spec_helper_local.rb
@@ -19,10 +19,3 @@ RSpec.configure do |_c|
     # ignore errors thrown while setting up mocks
   end
 end
-
-# We need this because the RAL uses 'should' as a method.  This
-# allows us the same behaviour but with a different method name.
-class Object
-  alias must should
-  alias must_not should_not
-end
